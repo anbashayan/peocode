804af6ae2cc1e4745fd2e7b49c52f8dbbd93c2a5
anbashayan <anbashayan12@gmail.com>
2023-07-21 15:58:55 +0530

    Peocode_2nd_session

---
+++ main2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+
+    //int a = 3;
+    //float b = 3.14;
+    //double v = 35.678;
+
+    //char c = 'a';
+
+    int num_array[] = {1,2,3,4,5};
+    //printf("a = %d\n",num_array[0]);
+
+    char name[] = "Anbashayan";
+    char name2[] = {'k','s','n'};
+
+    printf("name = %s\n",name2);
+
+}
